6fae43928132ca09dac7b07f1a59698cf27bdab5
pyos <pyos100500@gmail.com>
2016-10-04 00:59:18 +0300

    Actually, add an option to intercept libc functions.

--- tests/cno.c
+++ tests/cno.c
@@ -1,4 +1,5 @@
 #define CONE_COMAIN
+#define COIL_INTERCEPT_DYNAMIC_LIBC 1
 #include "../cone/coin.h"
 #include "../cone/coil.h"
 
@@ -31,7 +32,7 @@ struct cno_data_t {
 
 static int writeall(struct cno_data_t *d, const char *data, size_t size) {
     while (size) {
-        ssize_t w = coil_write(d->fd, data, size);
+        ssize_t w = write(d->fd, data, size);
         if (w <= 0)
             return CNO_ERROR(TRANSPORT, "could not write to socket");
         data += w;
@@ -63,7 +64,7 @@ static int handle_connection(int client) {
         goto error;
     ssize_t rd;
     char data[4096];
-    while ((rd = coil_read(client, data, sizeof(data))))
+    while ((rd = read(client, data, sizeof(data))))
         if (rd < 0 || cno_connection_data_received(&d.conn, data, (size_t) rd))
             goto error;
     if (cno_connection_lost(&d.conn))
@@ -87,9 +88,9 @@ int comain(int argc, const char **argv) {
     servaddr.sin_addr.s_addr = htons(INADDR_ANY);
     servaddr.sin_port = htons(8000);
     bind(fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-    coil_listen(fd, 127);
+    listen(fd, 127);
     int client;
-    while ((client = coil_accept(fd, NULL, NULL)) >= 0 || errno == ECONNABORTED)
+    while ((client = accept(fd, NULL, NULL)) >= 0 || errno == ECONNABORTED)
         cone_decref(cone(&handle_connection, (void*)(long)client));
     if (errno != EINVAL)
         perror("accept");
