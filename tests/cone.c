fb5ee12dc5abcc7523e9038935364cf576645bd0
pyos <pyos100500@gmail.com>
2016-10-05 23:04:15 +0300

    Run each test in a separate event loop.

--- tests/cone.c
+++ tests/cone.c
@@ -5,35 +5,21 @@ struct args { unsigned N; int fd; const char *data; size_t size; };
 
 static int writer(struct args *aptr) {
     struct args args = *aptr;
-    for (unsigned i = 0; i < args.N; i++)
-        if (write(args.fd, args.data, args.size) < 0)
+    for (ssize_t wr, mod = 0; args.N--; mod = (mod + wr) % args.size)
+        if ((wr = write(args.fd, args.data + mod, args.size - mod)) < 0)
             return mun_error_os();
     return mun_ok;
 }
 
 static int reader(struct args *aptr) {
     struct args args = *aptr;
-    ssize_t rd, mod = 0;
     char buf[args.size];
-    for (unsigned i = 0; i < args.N; i++) {
+    for (ssize_t rd, mod = 0; args.N--; mod = (mod + rd) % args.size) {
         if ((rd = read(args.fd, buf, args.size - mod)) < 0)
             return mun_error_os();
         if (memcmp(args.data + mod, buf, rd))
             return mun_error(assert, "recvd bad data");
-        mod = (mod + rd) % args.size;
     }
-    return mun_ok;
-}
-
-static int rdwr_inner(int *fds) {
-    if (cone_unblock(fds[0]) || cone_unblock(fds[1]))
-        return mun_error_up();
-    struct args ar = { 100000, fds[0], "Hello, World! Hello, World! Hello, World! Hello, World!", 55 };
-    struct args aw = { 100000, fds[1], "Hello, World! Hello, World! Hello, World! Hello, World!", 55 };
-    struct cone *r = cone(reader, &ar);
-    struct cone *w = cone(writer, &aw);
-    if (cone_join(r) || cone_join(w))
-        return mun_error_up();
     return mun_ok;
 }
 
@@ -41,20 +27,24 @@ static int rdwr() {
     int fds[2];
     if (pipe(fds))
         return mun_error_os();
-    if (cone_root(0, cone_bind(&rdwr_inner, fds)))
-        return mun_error_up();
-    close(fds[0]);
-    close(fds[1]);
-    return mun_ok;
+    if (cone_unblock(fds[0]) || cone_unblock(fds[1]))
+        return close(fds[0]), close(fds[1]), mun_error_up();
+    struct args ar = { 100000, fds[0], "Hello, World! Hello, World! Hello, World! Hello, World!", 55 };
+    struct args aw = { 100000, fds[1], "Hello, World! Hello, World! Hello, World! Hello, World!", 55 };
+    struct cone *r = cone(reader, &ar);
+    struct cone *w = cone(writer, &aw);
+    if (cone_join(r) || cone_join(w))
+        return close(fds[0]), close(fds[1]), mun_error_up();
+    return close(fds[0]), close(fds[1]), mun_ok;
 }
 
-static int yield(char *msg, size_t limit) {
+static int yield(char *msg) {
     const unsigned N = 1000000;
     mun_nsec a = mun_nsec_monotonic();
     for (unsigned i = 0; i < N; i++)
         sched_yield();
     mun_nsec b = mun_nsec_monotonic();
-    snprintf(msg, limit, "%f ns", mun_u128_to_double(mun_u128_sub(b, a)) / N);
+    sprintf(msg, "%f ns", mun_u128_to_double(mun_u128_sub(b, a)) / N);
     return mun_ok;
 }
 
