f4a6f2b37944fa645a7209df8c96029e5486bedc
pyos <pyos100500@gmail.com>
2016-10-15 19:11:06 +0300

    [deck] add some comments, too.

--- deck.c
+++ deck.c
@@ -1,7 +1,15 @@
+//
+// deck // distributed lock
+//
+// See deck.h for API.
+//
 #include "deck.h"
+
 #if DECK_DEBUG
 #include <stdio.h>
 #include <inttypes.h>
+// Write a diagnostic message to stderr with the current PID and timestamps (both real
+// and logical). Used for consistency tests.
 #define deck_debug_msg(t, pid, fmt, ...) fprintf(stderr, "[%" PRIu64 "|%u] %u: " fmt "\n", mun_usec_now(), t, pid, ##__VA_ARGS__)
 #else
 static inline void deck_debug_msg() {}
@@ -17,17 +25,71 @@ enum deck_state
 
 struct deck_nero
 {
-    uint32_t pid;
     struct nero *rpc;
     const char *request;
     const char *release;
+    // Apparent ID of the remote side.
+    uint32_t pid;
 };
 
 struct deck_request
 {
-    uint32_t pid;
-    uint32_t time;
+    // Both describe the state of the side that sent the request.
+    uint32_t pid, time;
 };
+
+// If the lock has been acquired and there's a blocked `deck_acquire`, wake it up.
+// No-op if not holding the lock yet.
+//
+// Errors: `memory`.
+//
+static int deck_wake(struct deck *lk) {
+    if (!(lk->state & DECK_REQUESTED) || !deck_acquired(lk))
+        return 0;
+    lk->state &= ~DECK_REQUESTED;
+    deck_debug_msg(++lk->time, lk->pid, "acquire");
+    return cone_event_emit(&lk->wake);
+}
+
+// Parse the remote state, update the local Lamport clock, write the new value to the output.
+//
+// Errors: `memory`.
+//
+static int deck_remote_clock(struct nero *rpc, struct deck *lk, struct romp *in, struct romp *out, struct deck_request *rq) {
+    if (romp_decode(in, "u4 u4", &rq->pid, &rq->time))
+        return -1;
+    lk->rpcs.data[mun_vec_find(&lk->rpcs, _->rpc == rpc)].pid = rq->pid;
+    return romp_encode(out, "u4", lk->time = (rq->time > lk->time ? rq->time : lk->time) + 1);
+}
+
+// nero endpoint: push a lock acquisition request to our queue.
+//
+// Errors: `memory`.
+//
+static int deck_remote_request(struct nero *rpc, struct deck *lk, struct romp *in, struct romp *out) {
+    struct deck_request rq = {};
+    if (deck_remote_clock(rpc, lk, in, out, &rq) MUN_RETHROW)
+        return -1;
+    deck_debug_msg(lk->time, rq.pid, "request");
+    unsigned i = mun_vec_bisect(&lk->queue, rq.time < _->time || (rq.time == _->time && rq.pid < _->pid));
+    return mun_vec_insert(&lk->queue, i, &rq);
+}
+
+// nero endpoint: remove a lock acquisition request from the queue. No-op if there was no request.
+//
+// Errors: `memory`.
+//
+static int deck_remote_release(struct nero *rpc, struct deck *lk, struct romp *in, struct romp *out) {
+    struct deck_request rq = {};
+    if (deck_remote_clock(rpc, lk, in, out, &rq) MUN_RETHROW)
+        return -1;
+    unsigned i = mun_vec_find(&lk->queue, _->pid == rq.pid);
+    if (i == lk->queue.size)
+        return 0;
+    deck_debug_msg(lk->time, rq.pid, "release");
+    mun_vec_erase(&lk->queue, i, 1);
+    return i == 0 ? deck_wake(lk) : 0;
+}
 
 struct deck_reqptr
 {
@@ -37,62 +99,11 @@ struct deck_reqptr
     const char *function;
 };
 
-void deck_fini(struct deck *lk) {
-    lk->state |= DECK_CANCELLED;
-    for (unsigned i = 0; i < lk->rpcs.size; i++) {
-        nero_del(lk->rpcs.data[i].rpc, lk->rpcs.data[i].request);
-        nero_del(lk->rpcs.data[i].rpc, lk->rpcs.data[i].release);
-    }
-    mun_vec_fini(&lk->rpcs);
-    mun_vec_fini(&lk->queue);
-    cone_event_emit(&lk->wake);
-}
-
-int deck_acquired(struct deck *lk) {
-    return lk->state & DECK_ACKED && lk->queue.data[0].pid == lk->pid;
-}
-
-static int deck_wake(struct deck *lk) {
-    if (!(lk->state & DECK_REQUESTED) || !deck_acquired(lk))
-        return 0;
-    lk->state &= ~DECK_REQUESTED;
-    deck_debug_msg(++lk->time, lk->pid, "acquire");
-    return cone_event_emit(&lk->wake);
-}
-
-static unsigned deck_bisect(struct deck *lk, struct deck_request rq) {
-    return mun_vec_bisect(&lk->queue, rq.time < _->time || (rq.time == _->time && rq.pid < _->pid));
-}
-
-typedef int deck_remote_fn(struct nero *, struct deck *, struct romp *, struct romp* );
-
-static int deck_remote_clock(struct nero *rpc, struct deck *lk, struct romp *in, struct romp *out, struct deck_request *rq) {
-    if (romp_decode(in, "u4 u4", &rq->pid, &rq->time))
-        return -1;
-    lk->rpcs.data[mun_vec_find(&lk->rpcs, _->rpc == rpc)].pid = rq->pid;
-    return romp_encode(out, "u4", lk->time = (rq->time > lk->time ? rq->time : lk->time) + 1);
-}
-
-static int deck_remote_request(struct nero *rpc, struct deck *lk, struct romp *in, struct romp *out) {
-    struct deck_request rq = {};
-    if (deck_remote_clock(rpc, lk, in, out, &rq) MUN_RETHROW)
-        return -1;
-    deck_debug_msg(lk->time, rq.pid, "request");
-    return mun_vec_insert(&lk->queue, deck_bisect(lk, rq), &rq);
-}
-
-static int deck_remote_release(struct nero *rpc, struct deck *lk, struct romp *in, struct romp *out) {
-    struct deck_request rq = {};
-    if (deck_remote_clock(rpc, lk, in, out, &rq) MUN_RETHROW)
-        return -1;
-    unsigned i = mun_vec_find(&lk->queue, _->pid == rq.pid);
-    if (i == lk->queue.size)
-        return mun_error(nero_protocol, "%u: %u did not request this lock", lk->pid, rq.pid);
-    deck_debug_msg(lk->time, rq.pid, "release");
-    mun_vec_erase(&lk->queue, i, 1);
-    return i == 0 ? deck_wake(lk) : 0;
-}
-
+// Call a remote method on a single peer, update the local clock from the result.
+// Arguments are packed into a single struct to allow running in a separate coroutine.
+//
+// Errors: `os`, `memory`.
+//
 static int deck_call_one(struct deck_reqptr *rp) {
     uint32_t time = 0;
     if (nero_call(rp->rpc, rp->function, "u4 u4", rp->rq.pid, rp->rq.time, "u4", &time) MUN_RETHROW)
@@ -101,13 +112,17 @@ static int deck_call_one(struct deck_reqptr *rp) {
     return 0;
 }
 
-static int deck_call_all(struct deck *lk, deck_remote_fn *f, struct deck_request rq) {
+// Call a remote method on all peers, fail if any of the requests do.
+//
+// Errors: `os`, `memory`.
+//
+static int deck_call_all(struct deck *lk, int is_release, struct deck_request rq) {
     int fail = 0;
     struct deck_reqptr reqs[lk->rpcs.size];
     for (unsigned i = 0; i < lk->rpcs.size; i++)
-        reqs[i] = (struct deck_reqptr){lk, lk->rpcs.data[i].rpc, rq,
-            f == deck_remote_request ? lk->rpcs.data[i].request
-          : f == deck_remote_release ? lk->rpcs.data[i].release : NULL
+        reqs[i] = (struct deck_reqptr){
+            lk, lk->rpcs.data[i].rpc, rq,
+            is_release ? lk->rpcs.data[i].release : lk->rpcs.data[i].request
         };
     struct cone *tasks[lk->rpcs.size];
     for (unsigned i = 0; i < lk->rpcs.size; i++)
@@ -121,12 +136,23 @@ static int deck_call_all(struct deck *lk, deck_remote_fn *f, struct deck_request
     return fail;
 }
 
+void deck_fini(struct deck *lk) {
+    lk->state |= DECK_CANCELLED;
+    for (unsigned i = 0; i < lk->rpcs.size; i++) {
+        nero_del(lk->rpcs.data[i].rpc, lk->rpcs.data[i].request);
+        nero_del(lk->rpcs.data[i].rpc, lk->rpcs.data[i].release);
+    }
+    mun_vec_fini(&lk->rpcs);
+    mun_vec_fini(&lk->queue);
+    cone_event_emit(&lk->wake);
+}
+
 int deck_add(struct deck *lk, struct nero *rpc, const char *request, const char *release) {
     struct nero_closure methods[] = {
         nero_closure(request, &deck_remote_request, lk),
         nero_closure(release, &deck_remote_release, lk),
     };
-    struct deck_nero desc = {lk->pid, rpc, request, release};
+    struct deck_nero desc = {rpc, request, release, lk->pid};
     if (mun_vec_reserve(&lk->rpcs, 1) || nero_add(rpc, methods, 2) MUN_RETHROW)
         return -1;
     mun_vec_append(&lk->rpcs, &desc);
@@ -143,6 +169,8 @@ void deck_del(struct deck *lk, struct nero *rpc) {
         unsigned j = mun_vec_find(&lk->queue, _->pid == pid);
         if (j != lk->queue.size)
             mun_vec_erase(&lk->queue, j, 1);
+        if (j == 0)
+            deck_wake(lk);
     }
 }
 
@@ -151,7 +179,7 @@ static int deck_release_impl(struct deck *lk, const char *msg) {
     deck_debug_msg(lk->time, lk->pid, "%s", msg);
     lk->state &= ~DECK_ACKED;
     mun_vec_erase(&lk->queue, mun_vec_find(&lk->queue, _->pid == lk->pid), 1);
-    return deck_call_all(lk, &deck_remote_release, crq);
+    return deck_call_all(lk, 1, crq);
 }
 
 int deck_acquire(struct deck *lk) {
@@ -164,7 +192,7 @@ int deck_acquire(struct deck *lk) {
                 return -1;
             deck_debug_msg(lk->time, lk->pid, "request");
             lk->state |= DECK_REQUESTED;
-            if (deck_call_all(lk, &deck_remote_request, arq))
+            if (deck_call_all(lk, 0, arq))
                 return lk->state &= ~DECK_REQUESTED, deck_release_impl(lk, "cancel"), -1;
             lk->state |= DECK_ACKED;
             if (deck_wake(lk) MUN_RETHROW)
@@ -180,4 +208,8 @@ int deck_release(struct deck *lk) {
     if (!deck_acquired(lk))
         return mun_error(assert, "not holding this lock");
     return --lk->state & DECK_RECURSION ? 0 : deck_release_impl(lk, "release");
+}
+
+int deck_acquired(struct deck *lk) {
+    return lk->state & DECK_ACKED && lk->queue.data[0].pid == lk->pid;
 }
--- deck.h
+++ deck.h
@@ -1,9 +1,17 @@
 #pragma once
+//
+// deck // distributed lock
+//
 #include "mun.h"
 #include "cone.h"
 #include "romp.h"
 #include "nero.h"
 
+// The lock.
+//
+// Initializer: designated; `pid` = some unique number, zero-initialize the rest.
+// Finalizer: `deck_fini`.
+//
 struct deck
 {
     uint32_t pid;
@@ -14,9 +22,34 @@ struct deck
     struct mun_vec(struct deck_request) queue;
 };
 
-void deck_fini    (struct deck *);
-int  deck_add     (struct deck *, struct nero *, const char *request, const char *release);
-void deck_del     (struct deck *, struct nero *);
-int  deck_acquire (struct deck *);
-int  deck_release (struct deck *);
-int  deck_acquired(struct deck *d);
+// Finalizer of `struct deck`. Single use.
+void deck_fini(struct deck *);
+
+// Add another participant in the lock. Behavior is undefined if lock has been requested,
+// but not released yet. `request` and `release` must survive until `deck_del` or `deck_fini`.
+//
+// Errors:
+//     `memory`: not enough space to record connection metadata.
+//
+int deck_add(struct deck *, struct nero *, const char *request, const char *release);
+
+// Forget about an RPC channel. Behavior is undefined if it was never added.
+// Other that that, should be OK to call at any point.
+void deck_del(struct deck *, struct nero *);
+
+// Request the lock and block until it is acquired. Or just block if another coroutine
+// has already sent a request.
+//
+// Errors:
+//     `memory`: could not add the request to the queue;
+//     `memory`: see `cone_spawn`, `cone_wait`, and `cone_event_emit`;
+//     possibly other errors returned by the remote side. All of them are exceptional.
+//
+int deck_acquire(struct deck *);
+
+// Undo one call to `deck_acquire`. The lock is recursive, so will not be released
+// until this function has been called the same number of times as `deck_acquire` has.
+int deck_release(struct deck *);
+
+// Return 1 if `deck_acquire` would not block.
+int deck_acquired(struct deck *d);
