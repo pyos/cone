598c48d3c0cb0349f8434f5174dbbc40c549940f
pyos <pyos100500@gmail.com>
2016-10-16 13:03:24 +0300

    [tests] display the messages on failed tests, too.

--- tests/base.c
+++ tests/base.c
@@ -15,9 +15,9 @@ int comain()
     for (unsigned i = 0; i < sizeof(__tests) / sizeof(*__tests); i++) {
         buf[0] = 0;
         printf("\033[33;1m * \033[0m\033[1m%s\033[0m\n", __tests[i].name);
-        if (!cone_root(0, cone_bind(__tests[i].impl, buf)))
-            printf("\033[A\033[32;1m + \033[0m\033[1m%s\033[0m\033[K%s%s\n", __tests[i].name, buf[0] ? ": " : "", buf);
-        else
+        int fail = cone_root(0, cone_bind(__tests[i].impl, buf));
+        printf("\033[A\033[3%d;1m * \033[0m\033[1m%s\033[0m\033[K%s%s\n", 1 + !fail, __tests[i].name, buf[0] ? ": " : "", buf);
+        if (fail)
             mun_error_show("test failed with", NULL);
     }
     return 0;
