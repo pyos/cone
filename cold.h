cb769063cb9170937e9b938dbcc8db46d143dc8a
dawg <pyos100500@gmail.com>
2018-08-27 22:27:13 +0300

    Shorter (in bytes, at least)

--- cold.h
+++ cold.h
@@ -10,36 +10,36 @@ struct iovec;
 struct msghdr;
 
 #if COLD_NO_OVERRIDE
-#define cold_fwd(ret, f, typed, untyped) ret cold_##f typed;
+#define cold_fwd(ret, f, args) ret cold_##f args
 #else
-#define cold_fwd(ret, f, typed, untyped) ret f typed; static inline ret cold_##f typed { return f untyped; }
+#define cold_fwd(ret, f, args) ret f args; static ret (*const cold_##f) args = &f
 #endif
 
 // Switch a file descriptor into non-blocking mode. See fcntl(2) for error codes.
 int cold_unblock(int fd);
 
 // `fd` is automatically switched to non-blocking mode:
-cold_fwd(int, listen, (int fd, int backlog), (fd, backlog))
+cold_fwd(int, listen, (int, int));
 // Coroutine-blocking; `fd` is automatically switched to non-blocking mode:
-cold_fwd(int, connect, (int fd, const struct sockaddr *addr, socklen_t addrlen), (fd, addr, addrlen))
+cold_fwd(int, connect, (int, const struct sockaddr *, socklen_t));
 // Returned socket is non-blocking:
-cold_fwd(int, accept, (int fd, struct sockaddr *addr, socklen_t *addrlen), (fd, addr, addrlen))
+cold_fwd(int, accept, (int, struct sockaddr *, socklen_t *));
 #ifdef __linux__
-cold_fwd(int, accept4, (int fd, struct sockaddr *addr, socklen_t *addrlen, int flags), (fd, addr, addrlen, flags))
+cold_fwd(int, accept4, (int, struct sockaddr *, socklen_t *, int));
 #endif
 // Coroutine-blocking versions:
-cold_fwd(ssize_t, read, (int fd, void *buf, size_t count), (fd, buf, count))
-cold_fwd(ssize_t, pread, (int fd, void *buf, size_t count, off_t offset), (fd, buf, count, offset))
-cold_fwd(ssize_t, readv, (int fd, const struct iovec *iov, int iovcnt), (fd, iov, iovcnt))
-cold_fwd(ssize_t, recv, (int fd, void *buf, size_t len, int flags), (fd, buf, len, flags))
-cold_fwd(ssize_t, recvfrom, (int fd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen), (fd, buf, len, flags, src_addr, addrlen))
-cold_fwd(ssize_t, recvmsg, (int fd, struct msghdr *msg, int flags), (fd, msg, flags))
-cold_fwd(ssize_t, write, (int fd, const void *buf, size_t count), (fd, buf, count))
-cold_fwd(ssize_t, pwrite, (int fd, const void *buf, size_t count, off_t offset), (fd, buf, count, offset))
-cold_fwd(ssize_t, writev, (int fd, const struct iovec *iov, int iovcnt), (fd, iov, iovcnt))
-cold_fwd(ssize_t, send, (int fd, const void *buf, size_t len, int flags), (fd, buf, len, flags))
-cold_fwd(ssize_t, sendto, (int fd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen), (fd, buf, len, flags, dest_addr, addrlen))
-cold_fwd(ssize_t, sendmsg, (int fd, const struct msghdr *msg, int flags), (fd, msg, flags))
+cold_fwd(ssize_t, read,     (int, void *, size_t));
+cold_fwd(ssize_t, pread,    (int, void *, size_t, off_t));
+cold_fwd(ssize_t, readv,    (int, const struct iovec *, int));
+cold_fwd(ssize_t, recv,     (int, void *, size_t, int));
+cold_fwd(ssize_t, recvfrom, (int, void *, size_t, int, struct sockaddr *, socklen_t *));
+cold_fwd(ssize_t, recvmsg,  (int, struct msghdr *, int));
+cold_fwd(ssize_t, write,    (int, const void *, size_t));
+cold_fwd(ssize_t, pwrite,   (int, const void *, size_t, off_t));
+cold_fwd(ssize_t, writev,   (int, const struct iovec *, int));
+cold_fwd(ssize_t, send,     (int, const void *, size_t, int));
+cold_fwd(ssize_t, sendto,   (int, const void *, size_t, int, const struct sockaddr *, socklen_t));
+cold_fwd(ssize_t, sendmsg,  (int, const struct msghdr *, int));
 
 #undef cold_fwd
 
