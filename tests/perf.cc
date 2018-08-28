c97f9054a8d3dc0204716b933f9406fc78969870
dawg <pyos100500@gmail.com>
2018-08-28 14:45:06 +0300

    Add another pointless perf test

--- tests/perf.cc
+++ tests/perf.cc
@@ -1,32 +1,60 @@
 #include <vector>
 
-template <unsigned N = 1000000, typename F>
-static bool run(char *msg, F&& f) {
+template <unsigned N, typename... Fs>
+static bool run(char *msg, Fs&&... fs) {
     auto a = cone::time::clock::now();
-    for (unsigned i = 0; i < N; i++)
-        if (!f())
+    bool results[] = {
+        [](auto&& f){
+            for (unsigned i = 0; i < N; i++)
+                if (!f(i))
+                    return false;
+            return true;
+        }(fs)...
+    };
+    for (bool c : results)
+        if (!c)
             return false;
     auto b = cone::time::clock::now();
     sprintf(msg, "%f us/iter", (double)std::chrono::duration_cast<std::chrono::microseconds>(b - a).count() / N);
     return true;
 }
 
+template <size_t yields>
 static bool test_yield(char *msg) {
-    return run(msg, cone::yield);
+    return run<yields>(msg, [](unsigned) { return cone::yield(); });
 }
 
+template <size_t cones>
 static bool test_spawn(char *msg) {
-    return run(msg, []() { return cone::ref{[](){ return true; }}->wait(); });
+    return run<cones>(msg, [](unsigned) { return cone::ref{[](){ return true; }}->wait(); });
 }
 
+template <size_t cones>
 static bool test_spawn_many(char *msg) {
-    std::vector<cone::ref> spawned;
-    spawned.reserve(1000000);
-    bool ok = run(msg, [&]() { spawned.emplace_back([]() { return true; }); return true; });
-    spawned.back()->wait();
-    return ok;
+    std::vector<cone::ref> spawned(cones);
+    return run<cones>(msg, [&](unsigned i) { spawned[i] = []() { return true; }; return true; },
+                           [&](unsigned i) { return spawned[i]->wait(); },
+                           [&](unsigned i) { spawned[i] = cone::ref(); return true; });
 }
 
-export { "perf:yield", &test_yield }
-     , { "perf:spawn", &test_spawn }
-     , { "perf:spawn many", &test_spawn_many }
+template <size_t cones, size_t yields_per_cone>
+static bool test_spawn_many_yielding(char *msg) {
+    std::vector<cone::ref> spawned(cones);
+    return run<cones>(msg,
+        [&](unsigned i) {
+            spawned[i] = []() {
+                for (size_t n = yields_per_cone; n--;)
+                    if (!cone::yield())
+                        return false;
+                return true;
+            };
+            return true;
+        },
+        [&](unsigned i) { return spawned[i]->wait(); },
+        [&](unsigned i) { spawned[i] = cone::ref(); return true; });
+}
+
+export { "perf:yield x 3m", &test_yield<3000000> }
+     , { "perf:(spawn(nop), wait, drop) x 6m", &test_spawn<6000000> }
+     , { "perf:spawn(nop) x 1m, wait x 1m, drop x 1m", &test_spawn_many<1000000> }
+     , { "perf:spawn(yield x 100) x 100k, wait x 100k, drop x 100k", &test_spawn_many_yielding<100000, 100> }
