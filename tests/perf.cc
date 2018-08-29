0c2f5ad664f0921c45778e125e1d9072f57e3c2d
dawg <pyos100500@gmail.com>
2018-08-29 13:58:59 +0300

    Add a mutex perf test

--- tests/perf.cc
+++ tests/perf.cc
@@ -54,7 +54,33 @@ static bool test_spawn_many_yielding(char *msg) {
         [&](unsigned i) { spawned[i] = cone::ref(); return true; });
 }
 
+template <size_t cones, size_t yields_per_cone>
+static bool test_mutex(char *msg) {
+    size_t r = 0;
+    cone::mutex m;
+    std::vector<cone::ref> spawned(cones);
+    return run<cones>(msg,
+        [&](unsigned i) {
+            spawned[i] = [&]() {
+                for (size_t n = yields_per_cone; n--;) if (cone::mutex::guard g{m}) {
+                    size_t c = r;
+                    if (!cone::yield())
+                        return false;
+                    r = c + 1;
+                } else {
+                    return false;
+                }
+                return true;
+            };
+            return true;
+        },
+        [&](unsigned i) { return spawned[i]->wait(); },
+        [&](unsigned i) { spawned[i] = cone::ref(); return true; })
+     && !mun_assert(r == cones * yields_per_cone, "%zu != %zu", r, cones * yields_per_cone);
+}
+
 export { "perf:yield x 3m", &test_yield<3000000> }
      , { "perf:(spawn(nop), wait, drop) x 6m", &test_spawn<6000000> }
      , { "perf:spawn(nop) x 1m, wait x 1m, drop x 1m", &test_spawn_many<1000000> }
      , { "perf:spawn(yield x 100) x 100k, wait x 100k, drop x 100k", &test_spawn_many_yielding<100000, 100> }
+     , { "perf:spawn((lock, yield, unlock) x 100) x 20k, wait x 20k, drop x 20k", &test_mutex<20000, 100> }
