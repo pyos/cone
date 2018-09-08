0ff6383277e5757eefc758100ce22451a4acc579
dawg <pyos100500@gmail.com>
2018-09-08 18:53:28 +0300

    Reduce the size of cone::ref to one pointer

--- cone.hh
+++ cone.hh
@@ -73,7 +73,7 @@ struct cone {
     }
 
     struct ref {
-        ref() noexcept {}
+        ref() noexcept = default;
 
         template <typename F /* = bool() */, typename G = std::remove_reference_t<F>>
         ref(F&& f, size_t stack = 100UL * 1024) noexcept {
@@ -95,11 +95,17 @@ struct cone {
         }
 
     protected:
-        std::unique_ptr<cone, void (*)(cone*) noexcept> r_{nullptr, cone_drop};
+        struct deleter {
+            void operator()(cone* c) const noexcept {
+                cone_drop(c);
+            }
+        };
+
+        std::unique_ptr<cone, deleter> r_;
     };
 
     struct thread : ref {
-        thread() noexcept {}
+        thread() noexcept = default;
 
         template <typename F /* = bool() */, typename G = std::remove_reference_t<F>>
         thread(F&& f, size_t stack = 100UL * 1024) noexcept {
@@ -125,14 +131,9 @@ struct cone {
             return !cone_wait_if(&e_, 1);
         }
 
-        template <typename F /* = bool() noexcept */>
+        template <bool b = true, typename F /* = bool() noexcept */>
         bool wait_if(F&& f) noexcept {
-            return !cone_wait_if(&e_, f());
-        }
-
-        template <typename F /* = bool() noexcept */>
-        bool wait_if_not(F&& f) noexcept {
-            return !cone_wait_if_not(&e_, f());
+            return b ? !cone_wait_if(&e_, f()) : !cone_wait_if_not(&e_, f());
         }
 
         void wake(size_t n = std::numeric_limits<size_t>::max()) noexcept {
@@ -149,7 +150,7 @@ struct cone {
         }
 
         bool lock() noexcept {
-            if (!try_lock()) while (!e_.wait_if_not([this]() { return try_lock(); })) {
+            if (!try_lock()) while (!e_.wait_if<false>([this]() { return try_lock(); })) {
                 if (mun_errno != EAGAIN) // could still be us who got woken by unlock() though
                     return try_lock() && (unlock(), false);
                 for (unsigned n = n_; n > 1 && !n_.compare_exchange_weak(n, n / 2);) {}
